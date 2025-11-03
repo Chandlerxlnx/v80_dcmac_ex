4526f47fc4a53832e9b52437ff505ff9f13488ca
Chandler Zhao <zhao_chunxiao@hotmail.com>
2025-11-03 21:06:21 +0800

    generate clib file from the sample test c code

---
+++ scripts/gen_sample_clib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+files=$(find ./dcmac_0_ex -name "dcmac*.c")
+
+echo ${files}
+
+for file in $files; do
+	# Get just the filename, without path
+	echo ${file}
+  filename=$(basename "$file")
+  echo ${filename}
+  echo ${filename%.c}
+  # Apply the changes and save to the current directory
+  sed "/main()/i /*" "$file" | sed "\$a */" > "./${filename%.c}_lib.h"
+done
+
+#sed '/main()/i /*' filename | sed '$a */' > newfile
